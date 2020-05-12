0d4ef8669002012bd00d024b44e4df59f1bc80a3
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-11 17:09:06 -0700

    recuder test 1 passes

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,1 +1,20 @@
+import { ADD_POST, DELETE_POST, UPDATE_POST } from '../actions/postActions';
 
+const initialState = [];
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADD_POST:
+      return [...state, action.payload];
+
+    case UPDATE_POST: 
+      return state.map((entry, i) => {
+        if(i === action.payload.index) return action.payload.entry;
+        return entry;
+      });
+    case DELETE_POST: 
+      return state.filter((_, i) => i !== action.payload);
+    default: 
+      return state;
+  }
+}
--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,2 +1,16 @@
+import { addPost, deletePost, updatePost } from '../actions/postActions';
+import reducer from '../reducers/postReducer';
 
+
+describe('post reducer', () => {
+  it('handle the ADD_POST action', () => {
+    const state = [];
+    const action = addPost({
+      title: 'entry',
+      body: 'entry entry'
+    });
+        
+    const newState = reducer(state, action);
+  });
+});
 
