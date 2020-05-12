4e3047bcce1039606357c490ca01ab0d318e3d70
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-11 17:17:12 -0700

    all reducer tests are passing

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { addPost, deletePost, updatePost } from '../actions/postActions';
 import reducer from '../reducers/postReducer';
 
@@ -11,6 +12,52 @@ describe('post reducer', () => {
     });
         
     const newState = reducer(state, action);
+
+    expect(newState).toEqual([
+      {
+        title: 'entry',
+        body:'entry entry'
+      }
+    ]);
+  });
+
+  it('handles the DELETE_POST action', () => {
+    const state = [
+      {
+        title: 'post title',
+        body: 'post body'
+      }
+    ];
+
+    const action = deletePost(0);
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([]);
   });
 });
+
+it('handle the UPDATE_POST action', () => {
+  const state = [
+    {
+      title: 'title',
+      body: 'body body '
+    }
+  ];
+
+  const action = updatePost(0, {
+    title: 'update title',
+    body: 'update body body'
+  });
+
+  const newState = reducer(state, action);
+
+  expect(newState).toEqual([
+    {
+      title: 'update title',
+      body: 'update body body'
+    }
+  ]);
+});
+
 
